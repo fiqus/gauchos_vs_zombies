adc39ea88b5e7eb427c812d5718f782e76c58fb7
Mariano Lambir <mlambir@gmail.com>
2022-05-13 17:27:16 -0300

    wip movement

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,42 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .add_system(sprite_movement)
         .run();
+}
+
+#[derive(Component)]
+struct Gaucho;
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite{
+                color: Color::rgb(0.25, 0.25, 0.75),
+                custom_size: Some(Vec2::new(10.0, 10.0)),
+                ..default()
+            },
+            transform: Transform::from_xyz(100., 0., 0.),
+            ..default()
+        })
+        .insert(Gaucho);
+}
+
+fn sprite_movement(time: Res<Time>, keyboard_input: Res<Input<KeyCode>>, mut sprite_position: Query<(&mut Gaucho, &mut Transform)>) {
+    for (_, mut transform) in sprite_position.iter_mut() {
+        if keyboard_input.pressed(KeyCode::Up) {
+            transform.translation.y += 10.0;
+        }
+        if keyboard_input.pressed(KeyCode::Down) {
+            transform.translation.y -= 10.0;
+        }
+        if keyboard_input.pressed(KeyCode::Left) {
+            transform.translation.x -= 10.0;
+        }
+        if keyboard_input.pressed(KeyCode::Right) {
+            transform.translation.x += 10.0;
+        }
+    }
 }
